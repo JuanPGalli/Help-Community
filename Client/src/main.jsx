882ca5e10b85dd89c013ed5d20455f7ce49713b8
Juan Pablo Galli <jpgallir@gmail.com>
2025-09-05 16:07:35 -0300

    React render method update to createRoot

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,19 +1,16 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
 
+const root = createRoot(document.getElementById('root'));
 
-
-ReactDOM.render(
-<Provider store={store}>
+root.render(
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </Provider>,
-  document.getElementById('root')
 );
