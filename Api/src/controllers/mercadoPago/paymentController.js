e600334e8099ecbd777042d452ef4c41458732ba
Juan Pablo Galli <jpgallir@gmail.com>
2025-09-07 00:34:41 -0300

    url update Mercado Pago back_urls and notification_url with my railway DB

--- Api/src/controllers/mercadoPago/paymentController.js
+++ Api/src/controllers/mercadoPago/paymentController.js
@@ -1,13 +1,12 @@
-const mercadopago = require("mercadopago");
-require("dotenv").config();
+const mercadopago = require('mercadopago');
+require('dotenv').config();
 
 const { ACCESS_TOKEN } = process.env;
 // * Configurar mercado pago
 // esto vendria a ser un post de creae una orden
 const createOrder = async (req, res) => {
   mercadopago.configure({
-    access_token:
-      "TEST-6850757546672488-100711-50177e634a355fb71db241599efdb0f3-1503945548",
+    access_token: ACCESS_TOKEN,
   });
   const pictureUrl = req.body[0].image;
   const products = req.body[0];
@@ -21,7 +20,7 @@ const createOrder = async (req, res) => {
         title: prod.product.name,
         id: prod.product.id,
         unit_price: prod.product.price,
-        currency_id: "ARS",
+        currency_id: 'ARS',
         quantity: prod.product.quantity || 1,
         picture_url: pictureUrl,
       };
@@ -35,7 +34,7 @@ const createOrder = async (req, res) => {
         title: req.body[0].name,
         id: req.body[0].id,
         unit_price: req.body[0].price,
-        currency_id: "ARS",
+        currency_id: 'ARS',
         quantity: req.body.quantity || 1,
         userEmail: req.body[0].email,
         picture_url: pictureUrl,
@@ -49,19 +48,18 @@ const createOrder = async (req, res) => {
 
     // ? Darle el control a mercado pago...
     back_urls: {
-      success: "https://help-community-production-ad63.up.railway.app/payment/success",
-      failure: "https://help-community-production-ad63.up.railway.app/payment/failure",
-      pending: "https://help-community-production-ad63.up.railway.app/payment/pending", //cuando el usuario no ha pagado
+      success: 'https://help-community-production.up.railway.app/payment/success',
+      failure: 'https://help-community-production.up.railway.app/payment/failure',
+      pending: 'https://help-community-production.up.railway.app/payment/pending', //cuando el usuario no ha pagado
     },
 
-    notification_url: `https://help-community-production-ad63.up.railway.app/payment/webhook?email=${email}`, 
-    
-    // ? Perdon Juan, copie mi puerto arriba del tuyo 
-    // ? Perdon Juan, copie mi puerto arriba del tuyo 
-    // ? Perdon Juan, copie mi puerto arriba del tuyo 
+    notification_url: `https://help-community-production.up.railway.app/payment/webhook?email=${email}`,
+
+    // ? Perdon Juan, copie mi puerto arriba del tuyo
+    // ? Perdon Juan, copie mi puerto arriba del tuyo
+    // ? Perdon Juan, copie mi puerto arriba del tuyo
     // notification_url: `https://pmcmwdd1-3001.brs.devtunnels.ms/payment/webhook?email=${email}`, // Puerto de gonzalo freddi si quieren probar cambien la url con su puerto https
     // notification_url: `https://312d05tj-3001.brs.devtunnels.ms/payment/webhook?email=${email}`, // Puerto de Lucas Caruso, idem Gonzalo Freddi ;)
-
   });
 
   res.send(result.body);
