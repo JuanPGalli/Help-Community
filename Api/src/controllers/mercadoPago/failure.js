a4fa0ed4933a485254b0cbd1ebda2c76b7c96911
Juan Pablo Galli <jpgallir@gmail.com>
2025-09-06 22:44:32 -0300

    url update Mercado Pago failure route

--- Api/src/controllers/mercadoPago/failure.js
+++ Api/src/controllers/mercadoPago/failure.js
@@ -1,6 +1,8 @@
 // * La información que envía mercado pago, la envía por query
-module.exports = (req,res)=> {
-    console.log(req.query);
-    // res.send('Pago rechazado')
-    res.redirect('https://help-community-theta.vercel.app')
+module.exports = (req, res) => {
+  console.log(req.query);
+  // res.send('Pago rechazado')
+  res.redirect('https://help-community-ecommerce.vercel.app/');
 };
+
+//'https://help-community-theta.vercel.app' --> deploy grupal
