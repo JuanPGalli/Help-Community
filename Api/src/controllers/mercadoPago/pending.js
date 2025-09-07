c0fcc3707ed9a96d3052cd48a4be3883f8f66d4a
Juan Pablo Galli <jpgallir@gmail.com>
2025-09-06 22:46:06 -0300

    url update MP pending route

--- Api/src/controllers/mercadoPago/pending.js
+++ Api/src/controllers/mercadoPago/pending.js
@@ -1,6 +1,8 @@
 // * La información que envía mercado pago, la envía por query
-module.exports = (req,res)=> {
-    console.log(req.query);
-    // res.send('Pago realizado')
-    res.redirect('https://help-community-theta.vercel.app')
+module.exports = (req, res) => {
+  console.log(req.query);
+  // res.send('Pago realizado')
+  res.redirect('https://help-community-ecommerce.vercel.app/');
 };
+
+//'https://help-community-theta.vercel.app' --> deploy grupal
