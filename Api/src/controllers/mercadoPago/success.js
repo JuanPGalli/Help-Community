6b7f1e7a76a09c24a9d9dc52226dfc35371cdd43
Juan Pablo Galli <jpgallir@gmail.com>
2025-09-08 16:11:07 -0300

    redirect payment to shoppinCart

--- Api/src/controllers/mercadoPago/success.js
+++ Api/src/controllers/mercadoPago/success.js
@@ -2,7 +2,7 @@
 module.exports = (req, res) => {
   console.log(req.query);
   // res.send('Pago realizado')
-  res.redirect('https://help-community-ecommerce.vercel.app/');
+  res.redirect('https://help-community-ecommerce.vercel.app/shoppingCart');
 };
 
 //'https://help-community-theta.vercel.app' --> deploy grupal
