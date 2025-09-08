49e2bb3054412a400001d67957ce3f72b5426b0c
Juan Pablo Galli <jpgallir@gmail.com>
2025-09-08 18:51:24 -0300

    fix camelCase attribute of React/JSX

--- Client/src/Components/loader/loader.jsx
+++ Client/src/Components/loader/loader.jsx
@@ -1,13 +1,23 @@
-import "./loader.css"
+import './loader.css';
 
-export default function Loader(){
-    return(<div>
-        <div class="showbox">
-        <div class="loader">
-          <svg class="circular" viewBox="25 25 50 50">
-         <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10"/>
-         </svg>
-         </div>
+export default function Loader() {
+  return (
+    <div>
+      <div className='showbox'>
+        <div className='loader'>
+          <svg className='circular' viewBox='25 25 50 50'>
+            <circle
+              className='path'
+              cx='50'
+              cy='50'
+              r='20'
+              fill='none'
+              strokeWidth='2'
+              strokeMiterlimit='10'
+            />
+          </svg>
         </div>
-    </div>)
+      </div>
+    </div>
+  );
 }
