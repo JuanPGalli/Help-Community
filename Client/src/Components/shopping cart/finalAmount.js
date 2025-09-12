df5b3ed89dbf0ee6c38528bcc06e66ab55d1d467
Juan Pablo Galli <jpgallir@gmail.com>
2025-09-12 20:08:45 -0300

    fix shoppingCart: final amount rounded to two decimals

--- Client/src/Components/shopping cart/finalAmount.js
+++ Client/src/Components/shopping cart/finalAmount.js
@@ -1,21 +1,18 @@
-
 // Esta función calculará el total de la compra
 // 1 bucle para recorrer todos los items que hay en el carrito
 // en cada vuelta accedo al produco y a la cantidad
-// multiplico para obtener el precio total de cada item 
-// sumo en cada vueltta el resultado acumulado
+// multiplico para obtener el precio total de cada item
+// sumo en cada vuelta el resultado acumulado
 
+export const finalAmount = (cartItems) => {
+  let totalAmount = 0;
 
-export const  finalAmount = (cartItems) => {
-    let totalAmount = 0;
+  for (let i = 0; i < cartItems.length; i++) {
+    const product = cartItems[i].product;
+    const quantity = cartItems[i].quantity;
+    const productTotal = Number((product.price * quantity).toFixed(2));
+    totalAmount += productTotal;
+  }
 
-    for (let i = 0; i < cartItems.length; i++) {
-      const product = cartItems[i].product;
-      const quantity = cartItems[i].quantity;
-      const productTotal = product.price  * quantity;
-      totalAmount += productTotal;
-    }
-    
-    return totalAmount;
-  };
-  
+  return totalAmount;
+};
