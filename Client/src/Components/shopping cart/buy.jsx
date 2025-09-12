b2b60a44f70ad3c41a593d59048938230398d2f8
Juan Pablo Galli <jpgallir@gmail.com>
2025-09-12 20:49:42 -0300

    fix styles

--- Client/src/Components/shopping cart/buy.jsx
+++ Client/src/Components/shopping cart/buy.jsx
@@ -1,35 +1,33 @@
-import { useEffect } from "react";
-import styles from "./buy.module.css";
-import { useSelector } from "react-redux/es/hooks/useSelector";
-import { finalAmount } from "./finalAmount"
-import { useAuth } from "../../context/AuthContext";
-import { useDispatch } from "react-redux";
-import { createOrder } from "../../redux/actions/action";
-
+import { useEffect } from 'react';
+import styles from './buy.module.css';
+import { useSelector } from 'react-redux/es/hooks/useSelector';
+import { finalAmount } from './finalAmount';
+import { useAuth } from '../../context/AuthContext';
+import { useDispatch } from 'react-redux';
+import { createOrder } from '../../redux/actions/action';
 
 export default function Buy() {
   const cart = useSelector((state) => state.cartShop);
   const totalProduct = cart.length;
-  const dispatch = useDispatch()
-  const auth = useAuth()
-  const email = auth.user.email
-  console.log(auth.user.email)
-  console.log(email)
+  const dispatch = useDispatch();
+  const auth = useAuth();
+  const email = auth.user.email;
+  console.log(auth.user.email);
+  console.log(email);
 
+  const handleSubmit = (detailProduct) => {
+    const allData = [[...cart], { email }, { total: totalAmount }];
+    console.log('allData de handleSubmit: ', allData);
+    dispatch(createOrder(allData));
+  };
 
-  const handleSubmit=(detailProduct)=>{
-    const allData = [[...cart],{email},{total: totalAmount}]
-    console.log("allData de handleSubmit: ", allData)
-    dispatch(createOrder(allData))
-}
+  useEffect(() => {
+    window.localStorage.setItem('dataCart', JSON.stringify(cart));
+  }, [cart]);
 
-  useEffect(()=>{
-    window.localStorage.setItem("dataCart", JSON.stringify(cart))
-  },[cart])
-
-  console.log(cart)
+  console.log(cart);
   const totalAmount = finalAmount(cart);
-  
+
   return (
     <div className={styles.buyCont}>
       <div className={styles.resume}>
@@ -42,10 +40,12 @@ export default function Buy() {
         </div>
         <div className={styles.amount}>
           <p className={styles.total}>Total</p>
-          <p className={styles.total}>{totalAmount}</p>
+          <p className={styles.total}>${totalAmount}</p>
         </div>
         <div className={styles.btn}>
-          <button onClick={handleSubmit}className={styles.btnBuy} disabled={email ? false : true} >Comprar</button>
+          <button onClick={handleSubmit} className={styles.btnBuy} disabled={email ? false : true}>
+            Comprar
+          </button>
         </div>
       </div>
     </div>
